6c632aec6c9dc8a1c20f9a3482734de9616a6204
Eliza Thomas <elizamthomas1994@gmail.com>
2023-03-22 16:33:12 +0000

    Created and test drove a Calculator class

--- chapter2/challenges/drills/lib/_2_classes.py
+++ chapter2/challenges/drills/lib/_2_classes.py
@@ -164,7 +164,21 @@ class StringFormatter():
 #   1
 #   > calculator.divide(6, 2)
 #   3.0
-
+class Calculator():
+  def __init__(self):
+    return None
+  
+  def add(self, num1, num2):
+    return num1 + num2
+  
+  def multiply(self, num1, num2):
+    return num1 * num2
+  
+  def subtract(self, num1, num2):
+    return num1 - num2
+  
+  def divide(self, num1, num2):
+    return num1 / num2
 
 
 # Class name: Apprentice
